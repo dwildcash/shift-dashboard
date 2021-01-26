fe4f9a1490e6cb4fef0e4a90bd2dcfd86fac1eaa
dwildcash <dwildcash@prontomail.com>
2021-01-25 19:58:11 -0500

    Make Delegate Auto Complete

--- shift-dashboard/Controllers/HomeController.cs
+++ shift-dashboard/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using shift_dashboard.Data;
+using shift_dashboard.Model;
 using shift_dashboard.Models;
 using System;
 using System.Collections.Generic;
@@ -11,16 +13,34 @@ namespace shift_dashboard.Controllers
 {
     public class HomeController : Controller
     {
+        private DashboardConfig _dashboardOptions;
         private readonly ILogger<HomeController> _logger;
+        private DashboardContext _dbcontext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DashboardConfig dashboardconfig, DashboardContext dbcontext)
         {
+            _dashboardOptions = dashboardconfig;
             _logger = logger;
+            _dbcontext = dbcontext;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+
+        [HttpGet]
+        public JsonResult GetDelegates()
+        {
+            if (!_dbcontext.Delegates.Any())
+            {
+                return null;
+            }
+            else
+            {
+                return Json(_dbcontext.Delegates.OrderBy(y => y.Username).Select(x => x.Username.TrimEnd().TrimStart()).ToList());
+            }
         }
 
         public IActionResult About()
